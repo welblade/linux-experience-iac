3695e9d13d7061ab01fd3e54c35f609fc50912ed
Wellington Oliveira dos Santos <wellington.oliveira.dev@gmail.com>
2022-08-12 00:59:32 -0300

    Tentando resolver o problema com os arrays

--- infrastart.sh
+++ infrastart.sh
@@ -1,13 +1,10 @@
 #!/bin/env bash
-declare -a GROUPS
-declare -a DIRS
-declare -a USERS
-GROUPS=("GRP_ADM" "GRP_VEN" "GRP_SEC")
-DIRS=("publico root 777" "adm GRP_ADM 770" "ven GRP_VEN 770" "sec GRP_SEC 770")
-USERS=("carlos GRP_ADM" "maria GRP_ADM" "joao GRP_ADM" "debora GRP_VEN" "sebastiana GRP_VEN" "roberto GRP_VEN" "josefina GRP_SEC" "amanda GRP_SEC" "rogerio GRP_SEC")
+declare -a groups=("GRP_ADM" "GRP_VEN" "GRP_SEC")
+declare -a dirs=("publico root 777" "adm GRP_ADM 770" "ven GRP_VEN 770" "sec GRP_SEC 770")
+declare -a users=("carlos GRP_ADM" "maria GRP_ADM" "joao GRP_ADM" "debora GRP_VEN" "sebastiana GRP_VEN" "roberto GRP_VEN" "josefina GRP_SEC" "amanda GRP_SEC" "rogerio GRP_SEC")
 
-echo "Criando os grupos" "${GROUPS[@]}"
-for group in "${GROUPS[@]}"
+echo "Criando os grupos" "${groups[@]}"
+for group in "${groups[@]}";
 do 
   if [ "$(getent group ${group})" ]; then
     echo "Não há necessidade de criar o grupo ${group}."
@@ -17,20 +14,18 @@ do
 done
 
 echo "Criando os diretórios."
-for t_dir in "${DIRS[@]}"
+for t_dir in "${dirs[@]}";
 do
-    declare -a dir_conf
-    dir_conf=("${t_dir}")
+    declare -a dir_conf=(${t_dir})
     mkdir -p "/shared/${dir_conf[0]}"
     chgrp "${dir_conf[1]}" "/shared/${dir_conf[0]}" 
     chmod "${dir_conf[2]}" "/shared/${dir_conf[0]}"
 done
 
 echo "Criando usuários."
-for t_user in "${USERS[@]}"
+for t_user in "${users[@]}";
 do
-    declare -a user_conf
-    user_conf=("${t_user}")
+    declare -a user_conf=(${t_user})
     useradd "${user_conf[0]}" -m -G "${user_conf[1]}" -s /bin/bash -p "$(openssl passwd -6 123459)" 
     usermod -e "${user_conf[0]}"
 done
