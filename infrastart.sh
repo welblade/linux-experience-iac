3218501439b7769e204d52a4c1a270c04cf7186d
Wellington Oliveira dos Santos <wellington.oliveira.dev@gmail.com>
2022-08-12 00:30:45 -0300

    Declaração das variaveis

--- infrastart.sh
+++ infrastart.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
+declade -a GROUPS
+declade -a DIRS
+declade -a USERS
 GROUPS=("GRP_ADM" "GRP_VEN" "GRP_SEC")
 DIRS=("publico root 777" "adm GRP_ADM 770" "ven GRP_VEN 770" "sec GRP_SEC 770")
 USERS=("carlos GRP_ADM" "maria GRP_ADM" "joao GRP_ADM" "debora GRP_VEN" "sebastiana GRP_VEN" "roberto GRP_VEN" "josefina GRP_SEC" "amanda GRP_SEC" "rogerio GRP_SEC")
@@ -16,6 +19,7 @@ done
 echo "Criando os diretórios."
 for t_dir in "${DIRS[@]}"
 do
+    declade -a dir_conf
     dir_conf=("${t_dir}")
     mkdir -p "/shared/${dir_conf[0]}"
     chgrp "${dir_conf[1]}" "/shared/${dir_conf[0]}" 
@@ -25,6 +29,7 @@ done
 echo "Criando usuários."
 for t_user in "${USERS[@]}"
 do
+    declade -a user_conf
     user_conf=("${t_user}")
     useradd "${user_conf[0]}" -m -G "${user_conf[1]}" -s /bin/bash -p "$(openssl passwd -6 123459)" 
     usermod -e "${user_conf[0]}"
