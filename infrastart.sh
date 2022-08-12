89b1ee8190e633fae67a41e11efbe049698561ed
Wellington Oliveira dos Santos <wellington.oliveira.dev@gmail.com>
2022-08-12 00:33:45 -0300

    Correção de sintaxe do comando declare

--- infrastart.sh
+++ infrastart.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
-declade -a GROUPS
-declade -a DIRS
-declade -a USERS
+declare -a GROUPS
+declare -a DIRS
+declare -a USERS
 GROUPS=("GRP_ADM" "GRP_VEN" "GRP_SEC")
 DIRS=("publico root 777" "adm GRP_ADM 770" "ven GRP_VEN 770" "sec GRP_SEC 770")
 USERS=("carlos GRP_ADM" "maria GRP_ADM" "joao GRP_ADM" "debora GRP_VEN" "sebastiana GRP_VEN" "roberto GRP_VEN" "josefina GRP_SEC" "amanda GRP_SEC" "rogerio GRP_SEC")
@@ -19,7 +19,7 @@ done
 echo "Criando os diretórios."
 for t_dir in "${DIRS[@]}"
 do
-    declade -a dir_conf
+    declare -a dir_conf
     dir_conf=("${t_dir}")
     mkdir -p "/shared/${dir_conf[0]}"
     chgrp "${dir_conf[1]}" "/shared/${dir_conf[0]}" 
@@ -29,7 +29,7 @@ done
 echo "Criando usuários."
 for t_user in "${USERS[@]}"
 do
-    declade -a user_conf
+    declare -a user_conf
     user_conf=("${t_user}")
     useradd "${user_conf[0]}" -m -G "${user_conf[1]}" -s /bin/bash -p "$(openssl passwd -6 123459)" 
     usermod -e "${user_conf[0]}"
